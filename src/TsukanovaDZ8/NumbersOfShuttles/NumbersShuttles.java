7d5254fa019055f6f6856c756e60244304a7e03b
Tsukanova Evgeniya <tata@gmail.com>
2023-04-23 01:17:18 +0300

    1 commit

--- src/TsukanovaDZ8/NumbersOfShuttles/NumbersShuttles.java
+++ src/TsukanovaDZ8/NumbersOfShuttles/NumbersShuttles.java
@@ -6,10 +6,12 @@ public class NumbersShuttles {
         while (true) {
             sumShuttles++;
             if (sumShuttles <= 101) {
-                for (int i = 1;i<=200; i++) {
+                for (int i = 1; i <= 200; i++) {
                     if (i == 4 || i == 9) {
                         continue;
                     } else if (i / 10 == 4 || i / 10 == 9) {
+                        continue;
+                    } else if (i / 10 == 14) {
                         continue;
                     } else if (i % 10 == 4 || i % 10 == 9) {
                         continue;
@@ -19,8 +21,7 @@ public class NumbersShuttles {
                         System.out.println(i);
                     }
                 }
-            }
-            else {
+            } else {
                 System.out.println("Прономеровано 100 шатлів, без 4 && 9");
                 break;
             }
