b97f7de36694a22804e7a5b5537cbbe0af8ede3f
Tsukanova Evgeniya <tata@gmail.com>
2023-04-23 20:32:56 +0300

    3 commit

--- src/TsukanovaDZ8/NumbersOfShuttles/NumbersShuttles.java
+++ src/TsukanovaDZ8/NumbersOfShuttles/NumbersShuttles.java
@@ -1,30 +1,28 @@
 package TsukanovaDZ8.NumbersOfShuttles;
 
-    public class NumbersShuttles {
-        public static void main(String[] args) {
-            int sumShuttles = 0;
-            while (true) {
-                if (sumShuttles <= 101) {
-                    for (int i = 1; i <= 200; i++) {
-                        if (i == 4 || i == 9) {
-                            continue;
-                        } else if (i / 10 == 4 || i / 10 == 9) {
-                            continue;
-                        } else if (i / 10 == 14) {
-                            continue;
-                        } else if (i % 10 == 4 || i % 10 == 9) {
-                            continue;
-                        }
-                        if (sumShuttles < 100) {
-                            sumShuttles++;
-                            System.out.println(i);
-                        }
+public class NumbersShuttles {
+    public static void main(String[] args) {
+        int sumShuttles = 0;
+        while (true) {
+            if (sumShuttles <= 99) {
+                for (int i = 1; i <= 200; i++) {
+                    if (i == 4 || i == 9) {
+                        continue;
+                    } else if (i / 10 == 4 || i / 10 == 9) {
+                        continue;
+                    } else if (i / 10 == 14) {
+                        continue;
+                    } else if (i % 10 == 4 || i % 10 == 9) {
+                        continue;
+                    }sumShuttles++;
+                    System.out.println(i);
+                    if (sumShuttles > 99) {
+                        System.out.println("Прономеровано 100 шатлів, без 4 && 9");
+                        break;
                     }
-                } else {
-                    System.out.println("Прономеровано 100 шатлів, без 4 && 9");
-                    break;
                 }
             }
         }
     }
+}
 
